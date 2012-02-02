8e079914028ff986e05e4a740e5b2fbac1726edf
Katherine Fleming <katherine.fleming@nrel.gov>
2012-02-02 21:43:42 +0000

    small change

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Dir[File.dirname(__FILE__) + "/../vendor/*"].each do |path|
+  gem_name = File.basename(path.gsub(/-\d+.\d+.\d+$/, ''))
+  gem_path = path + "/lib/" + gem_name + ".rb"
+  require gem_path if File.exists? gem_path
+end
+
 # Initialize the rails application
 Bemscape::Application.initialize!

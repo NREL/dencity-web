81afbcfe25083fbf360fa0cbc758815806cfd16d
Katherine Fleming <katherine.fleming@nrel.gov>
2012-02-07 20:37:29 +0000

    devise configurations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-=begin  
-  before_filter :authorize
 
-  protected
-
-  def authorize
-    redirect = true
-    @is_api = false
-    respond_to do |format|
-      format.html do
-        @is_api = false
-      end
-      format.xml do
-        @apiuser = User.authenticate(params[:api_key])
-        if @apiuser.nil?
-          render :xml => { :error => :unauthorized }, :status => :unauthorized 
-        else
-          @is_api = true
-        end
-      end
-    end
+  #devise default overrides
+  def after_sign_up_path_for(resource)
+    root_path
   end
-=end
- 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end

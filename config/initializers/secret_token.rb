3808e28e2dd24973450ec695798226b249540f3b
kflemin <katherine.fleming@nrel.gov>
2014-04-22 09:52:50 -0600

    adding new Rails4 on Jruby App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dencity::Application.config.secret_key_base = 'c16b2af941ef14e93e96c6ee21d5f04f661fde6729af9ddb6b69abaa5d9706da2ff7433dcc4802fd40cec30abb914c88eee46f63219db4498edb7dff407b2c68'

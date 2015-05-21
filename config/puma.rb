59a6cd9ed9c18bca0a0d9ceb3feeceafc43b6452
Nicholas Long <nicholas.long@nrel.gov>
2015-05-21 15:26:51 -0600

    use railsenv variable!

--- config/puma.rb
+++ config/puma.rb
@@ -3,11 +3,13 @@
 # start puma with:
 # puma -C ./config/puma.rb
 
+#application_path = '.'
+railsenv = ENV['RAILS_ENV']
 #directory application_path
-environment ENV['RAILS_ENV']
+environment railsenv
 daemonize true
 #pidfile "/srv/pids/puma-production.pid"
 #state_path "#{application_path}/tmp/pids/puma-#{railsenv}.state"
- stdout_redirect "/var/log/puma-#{railsenv}.stdout.log", "/var/log/puma-#{railsenv}.stderr.log"
+stdout_redirect "/var/log/puma-#{railsenv}.stdout.log", "/var/log/puma-#{railsenv}.stderr.log"
 threads 0, 16
 bind 'tcp://0.0.0.0:3000'

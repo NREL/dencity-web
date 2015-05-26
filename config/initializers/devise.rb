6169560c20d279609e43f3c5e32e8ac52da2669b
Nicholas Long <nicholas.long@nrel.gov>
2015-05-26 17:29:19 -0600

    lock out after 20 attempts

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
 
   # TODO: remove from repo!
-  config.secret_key = '06fafa6ac19076fcead9f0d6dcf5a0d624121946ee1d3052d964e897242f427ad5f554e8bed772684784e23b5c0c9abae624d8ec9a5c15fe083e633bd8efd378'
+  config.secret_key = '06fafa6ac19076fcead9f0d6dcf5a0d624321946ee1d3052d964e897242f427ad5f554e8bed772684784e23b5c0c9abae624d8ec9a5c15fe083e633bd8efd378'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
@@ -159,27 +159,27 @@ Devise.setup do |config|
   # Defines which strategy will be used to lock an account.
   # :failed_attempts = Locks an account after a number of failed attempts to sign in.
   # :none            = No lock strategy. You should handle locking by yourself.
-  # config.lock_strategy = :failed_attempts
+  config.lock_strategy = :failed_attempts
 
   # Defines which key will be used when locking and unlocking an account
-  # config.unlock_keys = [ :email ]
+  config.unlock_keys = [ :email ]
 
   # Defines which strategy will be used to unlock an account.
   # :email = Sends an unlock link to the user email
   # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
   # :both  = Enables both strategies
   # :none  = No unlock strategy. You should handle unlocking by yourself.
-  # config.unlock_strategy = :both
+  config.unlock_strategy = :both
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  # config.maximum_attempts = 20
+  config.maximum_attempts = 20
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
-  # config.unlock_in = 1.hour
+  config.unlock_in = 1.hour
 
   # Warn on the last attempt before the account is locked.
-  # config.last_attempt_warning = false
+  config.last_attempt_warning = false
 
   # ==> Configuration for :recoverable
   #

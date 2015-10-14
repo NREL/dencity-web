e850d2a97072f4edd56da38154eb929a331c76df
Nicholas Long <nicholas.long@nrel.gov>
2015-10-14 10:22:37 -0600

    add smtp settings to environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,25 @@ Dencity::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   Rails.application.routes.default_url_options[:host] = 'dencity.org'
 
-  config.action_mailer.delivery_method = :mailgun
+  if ENV['MAILGUN_SMTP_LOGIN'] && ENV['MAILGUN_SMTP_LOGIN'] != '' &&
+      ENV['MAILGUN_API_KEY'] && ENV['MAILGUN_API_KEY'] != ''
+    config.action_mailer.delivery_method = :mailgun
+  else
+    # fall back to SMTP with credentials defined in the environment
+    config.action_mailer.delivery_method = :smtp
+
+    # if using gmail make sure to enable less secure app
+    # (https://www.google.com/settings/security/lesssecureapps)
+    config.action_mailer.smtp_settings = {
+        :address => ENV['SMTP_ADDRESS'],
+        :port => ENV['SMTP_PORT'],
+        :user_name =>  ENV['SMTP_USERNAME'],
+        :password =>  ENV['SMTP_PASSWORD'],
+        :authentication => :login,
+        :enable_starttls_auto => true
+    }
+  end
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: Rails.application.routes.default_url_options[:host] }
 
